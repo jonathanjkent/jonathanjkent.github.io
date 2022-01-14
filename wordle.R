4cd0f8540dbbf6f7b8d186b2c30e8ec7ef02bae8
jonathanjkent <60342815+jonathanjkent@users.noreply.github.com>
2022-01-14 11:17:11 +0100

    clean more, add Catalan

--- wordle.R
+++ wordle.R
@@ -1,19 +1,32 @@
 library(tidyverse)
 library(stringr)
 
-# Download word list and limit to 5 letter words
-# English word list seems to be very similar, but not exactly the same, as the one used by Wordle
-# For Spanish, run commented line instead
-download.file("http://www.gwicks.net/textlists/usa.zip", words <- tempfile())
-# download.file("http://www.gwicks.net/textlists/espanol.zip", words <- tempfile())
-words <- read_lines(words)
-words <- words[str_length(words)==5]
+#### Pick English, Catalan, or Spanish and download list of 5 letter words ####
 
+get.words <- function(language){
+  if (language == "EN"){
+    download.file("http://www.gwicks.net/textlists/usa.zip", words <- tempfile())
+    words <- read_lines(words)
+    words <- toupper(unique(words[str_length(words)==5]))
+  }
+  if (language == "CAT"){
+    words <- read_delim("https://raw.githubusercontent.com/Softcatala/catalan-dict-tools/master/resultats/lt/diccionari.txt", delim = " ", col_names = F, col_types = cols()) %>% filter(str_detect(X1,"^[:lower:]+$") & str_length(X1) == 5)
+    words <- toupper(unique(stri_trans_general(words$X1, "Latin-ASCII")))
+  }
+  if (language == "ES"){
+    download.file("http://www.gwicks.net/textlists/espanol.zip", words <- tempfile())
+    words <- read_lines(words)
+    words <- toupper(unique(words[str_length(words)==5]))
+  }
+  return(words)
+}
 
-#### Play (more fun to check after playing, cheater)
-# Between daily Wordles, you can test here: http://foldr.moe/hello-wordl/
+words <- get.words("EN") # EN, CAT or ES
+
+#### Play (more fun to check after playing, cheater) ####
 
 # Create wordle function
+
 wordle <- function(word, colors){
   
   if (str_length(word) != 5) {stop("Word must be 5 letters.")}
@@ -24,30 +37,37 @@ wordle <- function(word, colors){
   gs <- str_which(unlist(str_split(colors,"")),"g")
   xs <- str_which(unlist(str_split(colors,"")),"x")
   ygs <- c(c(str_sub(word,ys,ys)),c(str_sub(word,gs,gs)))
+  x.ltrs <- c(str_sub(word,xs,xs))
   
+  # Remove words incompatible with yellow squares
   if (length(ys) > 0) {
     for (i in ys){remain <- str_subset(remain,str_sub(word,i,i))}
     for (i in ys){remain <- remain[str_which(str_sub(remain,i,i),str_sub(word,i,i), negate = T)]}
   }
   
+  # Remove words incompatible with green squares
   if (length(gs) > 0) {
     for (i in gs){remain <- remain[str_which(str_sub(remain,i,i),str_sub(word,i,i), negate = F)]}
   }
   
+  # Remove words incompatible with gray squares
   if (length(xs) > 0) {for (i in xs){
-    if (!str_sub(word,i,i) %in% ygs) {remain <- str_subset(remain,str_sub(word,i,i), negate = T)}
+    if (!str_sub(word,i,i) %in% ygs) {remain <- str_subset(remain,str_sub(word,i,i), negate = T)} else {remain[str_length(str_remove_all(remain,"s"))==(5-length(str_subset(ygs,str_sub(word,i,i))))]}
     }
   }
   
+  # Remove words incompatible with any double yellow/green letters
   if (length(ygs[duplicated(ygs)])>0) {
     for (i in ygs[duplicated(ygs)]) {
       remain <- remain[str_length(str_remove_all(remain,i)) < 4]
     }
   }
   
+  # Test each word for similarity with other remaining words
   for (i in remain){
     letters <- unique(unlist(str_split(i,"")))
     for (j in letters) {if (j == letters[1]) {test <- length(str_subset(remain,j))} else {test <- c(test, length(str_subset(remain,j)))}}
+    
     if (i == remain[1]) {tests <- sum(test)} else {tests <- c(tests, sum(test))}
   }
   
@@ -57,17 +77,20 @@ wordle <- function(word, colors){
 }
 
 # Run this line to start new game.
+
 remain <- words  
 
 # Run this to find remaining words after each guess. 
-# Colors are y for yellow, g for green, x for grey.
+# Colors are "y" for yellow, "g" for green, "x" for grey.
 # "Similarity" variable ranks possible guesses by how many letters (in any position) they have in common with other remaining words (z-score).
-remain <- wordle("talon","ggxxy")  
+
+remain <- wordle("RATES","ygxxx")  
 
 
-#### Find best first words (takes a little while, be patient)
+#### Find best first words (takes a little while, be patient) ####
 
 # Find how many letters each word matches with other words, in any position (yellows)
+
 for (i in words){
   
   letters <- unique(unlist(str_split(i,"")))
@@ -80,6 +103,7 @@ for (i in words){
 }
 
 # Find how many letters each word matches with other words, in the right position (greens)
+
 for (i in words){
  
   for (j in 1:5){
@@ -91,6 +115,7 @@ for (i in words){
 }
 
 # Arbitrarily add the two together and scale to make an index score
+
 print(first.words <- tibble(word = words, yellows = yellows, greens = greens) %>% mutate(index = as.numeric(scale(yellows+greens))) %>% arrange(desc(index)))
 
 
