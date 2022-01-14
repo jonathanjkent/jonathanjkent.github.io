118f4ed4ee5bda5e33f6e1f353735db5e6fb7de5
jonathanjkent <60342815+jonathanjkent@users.noreply.github.com>
2022-01-14 10:19:04 +0100

    cleaner code, add Spanish

--- wordle.R
+++ wordle.R
@@ -2,9 +2,12 @@ library(tidyverse)
 library(stringr)
 
 # Download word list and limit to 5 letter words
-# Word list seems to be very similar, but not exactly the same, as the one used by Wordle
+# English word list seems to be very similar, but not exactly the same, as the one used by Wordle
+# For Spanish, run commented line instead
 download.file("http://www.gwicks.net/textlists/usa.zip", words <- tempfile())
-words <- read_csv(words, col_types = cols()) %>% rename(word = "a") %>% filter(validUTF8(word)==T) %>% filter(str_length(word) == 5)
+# download.file("http://www.gwicks.net/textlists/espanol.zip", words <- tempfile())
+words <- read_lines(words)
+words <- words[str_length(words)==5]
 
 
 #### Play (more fun to check after playing, cheater)
@@ -54,46 +57,43 @@ wordle <- function(word, colors){
 }
 
 # Run this line to start new game.
-remain <- words$word  
+remain <- words  
 
 # Run this to find remaining words after each guess. 
 # Colors are y for yellow, g for green, x for grey.
 # "Similarity" variable ranks possible guesses by how many letters (in any position) they have in common with other remaining words (z-score).
-remain <- wordle("first","ygxxx")  
+remain <- wordle("talon","ggxxy")  
 
 
 #### Find best first words (takes a little while, be patient)
 
-words <- words %>% mutate(yellows = 0, greens = 0)
-
 # Find how many letters each word matches with other words, in any position (yellows)
-for (i in 1:nrow(words)){
+for (i in words){
   
-  letters <- unique(unlist(str_split(words$word[i],"")))
+  letters <- unique(unlist(str_split(i,"")))
   
   for (j in letters) {
-    
-    if (j == letters[1]) {yellow <- length(str_subset(words$word,j))} else {yellow <- c(yellow, length(str_subset(words$word,j)))}
+    if (j == letters[1]) {yellow <- length(str_subset(words,j))} else {yellow <- c(yellow, length(str_subset(words,j)))}
   }
   
-  words$yellows[i] <- sum(yellow)
+  if (i == words[1]) {yellows <- sum(yellow)} else {yellows <- c(yellows, sum(yellow))}
 }
 
 # Find how many letters each word matches with other words, in the right position (greens)
-for (i in 1:nrow(words)){
+for (i in words){
  
   for (j in 1:5){
-    if (j == 1) {green <- length(str_subset(str_sub(words$word,j,j),str_sub(words$word[i],j,j)))} 
-    else {green <- c(green, length(str_subset(str_sub(words$word,j,j),str_sub(words$word[i],j,j))))}
+    if (j == 1) {green <- length(str_subset(str_sub(words,j,j),str_sub(i,j,j)))} 
+    else {green <- c(green, length(str_subset(str_sub(words,j,j),str_sub(i,j,j))))}
   }
   
-  words$greens[i] <- sum(green)
+  if (i == words[1]) {greens <- sum(green)} else {greens <- c(greens, sum(green))}
 }
 
 # Arbitrarily add the two together and scale to make an index score
-words$index <- as.numeric(scale(words$yellows+words$greens))
+print(first.words <- tibble(word = words, yellows = yellows, greens = greens) %>% mutate(index = as.numeric(scale(yellows+greens))) %>% arrange(desc(index)))
 
-print(words %>% arrange(desc(index)))
+
 
 
 
